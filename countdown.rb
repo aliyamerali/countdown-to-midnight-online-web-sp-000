45a887dca3115e5fd6630addc5b2dbfb0a5fa471
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-19 03:27:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(num)
-  while countdown > 0
+  while num > 0
     puts "#{num} SECOND(S)!"
-    countdown -= 1
+    num -= 1
   end
   return "HAPPY NEW YEAR!"
 end
